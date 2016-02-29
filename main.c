cd9730e0594ad1003cb6cd0d7d05206dcd7170dd
mpharmon <mpharmon@email.arizona.edu>
2016-02-28 20:03:42 -0700

    First Draft of final main.c.  Waiting on clarification from professor as to checking PIN's against the PIN DB.  Also need to implement PIN DB.

--- main.c
+++ main.c
@@ -9,11 +9,13 @@
 typedef enum enumState{
   PRE_ENTER,
   ENTER,
+  ENTER_WAIT_RELEASE,
   ENTER_WAIT,
   ENTER_VALID,
   ENTER_INVALID,
   PRE_SET,
   SET,
+  SET_WAIT_RELEASE,
   SET_WAIT,
   SET_VALID,
   SET_INVALID
@@ -21,16 +23,10 @@ typedef enum enumState{
 
 volatile STATE state = PRE_ENTER;
 
-void main(){
+void main(void){
   int x;
   SYSTEMConfigPerformance(10000000);
   enableInterrupts();
-  TRISDbits.TRISD0 = 0;
-  TRISDbits.TRISD1 = 0;
-  TRISDbits.TRISD2 = 0;
-  LATDbits.LATD0 = 0;
-  LATDbits.LATD1 = 0;
-  LATDbits.LATD2 = 0;
   LCD_Init();
   KeyPad_Init();
   char lastChar = NULL;
@@ -39,94 +35,123 @@ void main(){
   while(1){
     switch(state){
       case PRE_ENTER:
-        LATDbits.LATD0 = 1;
-        LATDbits.LATD1 = 1;
-        LATDbits.LATD2 = 1;
         LCD_Clear();
-        //LCD_MoveCursor(1,1);
         LCD_PrintString("Enter PIN:");
+        LCD_MoveCursor(1,2);
+        numCount = 0;
+        currentChar = NULL;
+        lastChar = NULL;
         state = ENTER_WAIT;
         break;
       case ENTER:
-        LATDbits.LATD0 = 1;
-        LATDbits.LATD1 = 0;
-        LATDbits.LATD2 = 0;
         currentChar = KeyPad_Scan();
-        if(lastChar == '*' && currentChar == '*'){
-          state = PRE_SET;
-        }else if(lastChar == '*'){
-          state = ENTER_INVALID;
-        }else{
-          LCD_MoveCursor(numCount + 1,2);
-          if(numCount < 4){
+        if(numCount == 0){
+          if(currentChar == '*'){
+            state = ENTER_WAIT_RELEASE;
+          }else if(currentChar == '#'){
+            state = ENTER_INVALID;
+          }else{
             LCD_PrintChar(currentChar);
-            numCount++;
-          }else state = ENTER_VALID;
-          lastChar = currentChar;
+            state = ENTER_WAIT_RELEASE;
+          }
+        }else if(numCount == 1){
+          if(lastChar == '*' && currentChar == '*'){
+            state = PRE_SET;
+          }else if(( lastChar == '*' && currentChar != '*')|| (lastChar != '*' && currentChar == '*') || currentChar == '#'){
+            state = ENTER_INVALID;
+          }else{
+            LCD_PrintChar(currentChar);
+            state = ENTER_WAIT_RELEASE;
+          }
+        }else if(numCount == 2){
+          if(currentChar == '*' || currentChar == '#'){
+            state = ENTER_INVALID;
+          }else{
+            LCD_PrintChar(currentChar);
+            state = ENTER_WAIT_RELEASE;
+          }
+        }else if(numCount == 3){
+          if(currentChar == '*' || currentChar == '#'){
+            state = ENTER_INVALID;
+          }else{
+            LCD_PrintChar(currentChar);
+            delayMs(500);
+            state = ENTER_VALID;
+          }
         }
-        state = ENTER_WAIT;
+        numCount++;
+        lastChar = currentChar;
         break;
+      case ENTER_WAIT_RELEASE:
       case ENTER_WAIT:
-        LATDbits.LATD0 = 0;
-        LATDbits.LATD1 = 1;
-        LATDbits.LATD2 = 0;
+      case SET_WAIT:
+      case SET_WAIT_RELEASE:
         break;
       case ENTER_VALID:
         LCD_Clear();
-        //LCD_MoveCursor(1,1);
         LCD_PrintString("Valid");
         state = PRE_ENTER;
+        lastChar = NULL;
+        numCount = 0;
+        delayMs(2000);
         break;
       case ENTER_INVALID:
         LCD_Clear();
-        //LCD_MoveCursor(1,1);
         LCD_PrintString("Invalid");
         state = PRE_ENTER;
+        lastChar = NULL;
+        numCount = 0;
+        delayMs(2000);
         break;
       case PRE_SET:
+        numCount = 0;
+        lastChar = NULL;
+        currentChar = NULL;
         LCD_Clear();
-        //LCD_MoveCursor(1,1);
-        LCD_PrintString("Set Mode");
-        state = SET_WAIT;
+        LCD_PrintString("Set Mode:");
+        LCD_MoveCursor(1,2);
+        state = SET_WAIT_RELEASE;
         break;
       case SET:
+        delayMs(100);
         currentChar = KeyPad_Scan();
         if(currentChar == '*' || currentChar == '#'){
           state = SET_INVALID;
         }else{
-          if(numCount < 4){
+          if(numCount < 3){
             LCD_PrintChar(currentChar);
             numCount++;
+            state = SET_WAIT_RELEASE;
           }else{
             state = SET_VALID;
           }
         }
-        state = SET_WAIT;
-        break;
-      case SET_WAIT:
         break;
       case SET_VALID:
         LCD_Clear();
-        //LCD_MoveCursor(1,1);
         LCD_PrintString("Valid");
         state = PRE_ENTER;
+        lastChar = NULL;
+        numCount = 0;
+        delayMs(2000);
         break;
       case SET_INVALID:
         LCD_Clear();
-        //LCD_MoveCursor(1,1);
         LCD_PrintString("Invalid");
         state = PRE_ENTER;
+        lastChar = NULL;
+        numCount = 0;
+        delayMs(2000);
         break;
     }
   }
 }
 
 void __ISR(_CHANGE_NOTICE_VECTOR,IPL7SRS) _CNInterrupt(void){
-  PORTD;
+  PORTE;
   IFS1bits.CNEIF = 0;// Put Flag Down
-  LATDbits.LATD0 = 1;
-  LATDbits.LATD1 = 0;
-  LATDbits.LATD2 = 1;
   if(state == ENTER_WAIT)state = ENTER;
+  if(state == ENTER_WAIT_RELEASE)state = ENTER_WAIT;
   if(state == SET_WAIT)state = SET;
+  if(state == SET_WAIT_RELEASE)state = SET_WAIT;
 }
